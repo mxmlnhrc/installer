e4d4c0234013b22b974dd3aa6f64874157837b13
mxmlnhrc <maxihenri03@gmail.com>
2021-11-18 14:44:36 +0100

    added version 2

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo dnf install snapd
+sudo service snapd stop
+systemctl start snapd.service
+
+sudo snap install teams-insiders
+sudo snap install sweethome3d-homedesign
+sudo snap install libreoffice
+sudo snap install gimp
+sudo snap install freecad
+sudo snap install arduino
+sudo snap install vlc
+sudo snap install code
