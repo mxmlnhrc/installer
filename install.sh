65dd1293cde3d07c914c85b339aec7b30bc11550
mxmlnhrc <maxihenri03@gmail.com>
2021-11-18 15:07:40 +0100

    added Proccesssing-Core to install

--- install.sh
+++ install.sh
@@ -10,3 +10,8 @@ sudo snap install freecad
 sudo snap install arduino
 sudo snap install vlc
 sudo snap install code
+
+wget https://github.com/processing/processing/releases/download/processing-0270-3.5.4/processing-3.5.4-linux64.tgz
+tar -xvzf processing-3.5.4-linux64.tgz
+cd processing-3.5.4/
+sudo sh install.sh
